805c7e64a3c7f2c6690146e621932fb75bf1f941
Casey C <kcx1codes@gmail.com>
2024-08-11 13:41:17 -0400

    Feat: Adds example config with example options

--- pluginframework/configuration_example/qplug.lua
+++ pluginframework/configuration_example/qplug.lua
@@ -2,8 +2,12 @@
 return {
 	-- Set to a string if you want an external template. Can be a url or a path.
 	external_template = nil,
+	-- external_template = "https://github.com/qsys-plugins/BasePlugin",
+	-- external_template = "https://github.com/qsys-plugins/ExamplePlugin"
+	-- external_template = "https://bitbucket.org/qsc-communities/basicpluginframework/src/main/"
 
-	-- Which build tool to use. This can be a string or a function.
+	-- Assign to nil if you want use the builtin build tool.
+	-- Optionally, you can create your own. Here's an example using the original from QSC
 	build_tool = function()
 		local cmd = ".\\plugincompile|PLUGCC.exe . .\\plugin.lua"
 		os.execute(cmd)
