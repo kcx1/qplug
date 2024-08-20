4e3d237e4e9bfadfd73981e710afb21e33dfc02b
Casey C <kcx1codes@gmail.com>
2024-08-20 01:18:08 -0400

    Feat: Implements the build command

--- src/modules/cli/subcommands/build.rs
+++ src/modules/cli/subcommands/build.rs
@@ -1,9 +1,13 @@
 use clap::ValueEnum;
-use mlua::UserData;
-use std::env;
+use mlua::{Lua, UserData};
+use std::{env, path::PathBuf};
 
-use crate::lua::parser::merge_lua_files;
+use crate::{
+    files::find_marker_file,
+    lua::{info::PluginInfo, parser::merge_lua_files},
+};
 
+//TODO: Refactor this to a more central location.
 #[derive(ValueEnum, Clone, Debug)]
 #[clap(rename_all = "lower")]
 pub enum VersionType {
@@ -14,17 +18,39 @@ pub enum VersionType {
 }
 impl UserData for VersionType {}
 
-pub fn build(version: VersionType) {
-    match version {
-        VersionType::Major => println!("major"),
-        VersionType::Minor => println!("minor"),
-        VersionType::Patch => println!("patch"),
-        VersionType::Dev => println!("dev"),
-    }
-    // TODO: Figure out some magic to know where project root is in relationship to user's
-    // pwd
-    let root_path = env::current_dir()
+// Build steps:
+//     - Get info
+//     - Update info.version based on increment
+//     - Compile plugin using config.
+
+//TODO: Add compile command
+pub fn compile() {
+    let current_path = env::current_dir()
         .expect("Unable to get current directory. Please check your permissions.");
-    let plugin_path = root_path.join("plugin_src");
-    merge_lua_files(root_path, plugin_path).unwrap();
+    let marker = find_marker_file(current_path.as_path());
+    if marker.is_some() {
+        let root_path = marker.unwrap();
+        let plugin_path = root_path.join("plugin_src");
+        match merge_lua_files(root_path, plugin_path) {
+            Ok(_) => println!("Plugin updated successfully."),
+            Err(e) => println!("Failed to update plugin: {}", e),
+        }
+    } else {
+        println!(
+            "No plugin found. Please create a plugin first or navigate to a plugin directory."
+        );
+    }
+}
+
+//TODO: Add update command to only update version without compiling
+pub fn update_version(version: VersionType, info_path: PathBuf, lua: &Lua) {
+    let mut info = PluginInfo::get_struct(&info_path, lua).expect("Error getting plugin info.");
+    info = info.update(version).expect("Update failed.");
+    info.write(info_path, lua)
+        .expect("Error writing plugin info.");
+}
+
+pub fn build(version: VersionType, info_path: PathBuf, lua: &Lua) {
+    update_version(version, info_path, lua);
+    compile();
 }
