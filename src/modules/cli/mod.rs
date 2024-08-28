82b6778532ecea8d33bc92041afd9283bf9e6bae
Casey C <kcx1codes@gmail.com>
2024-08-28 17:47:26 -0400

    Refactor: Adds color to the cli output

--- src/modules/cli/mod.rs
+++ src/modules/cli/mod.rs
@@ -1,12 +1,28 @@
-use clap::{arg, value_parser, Arg, ArgAction, Command};
+use clap::{
+    arg, builder::styling, crate_authors, crate_version, value_parser, Arg, ArgAction, ColorChoice,
+    Command,
+};
 use clap_complete::Shell;
 use subcommands::build::VersionType;
 
 pub mod subcommands;
 
+const STYLES: styling::Styles = styling::Styles::styled()
+    .header(styling::AnsiColor::BrightWhite.on_default().bold())
+    .error(styling::AnsiColor::BrightRed.on_default().bold())
+    .usage(styling::AnsiColor::BrightWhite.on_default().bold())
+    .literal(styling::AnsiColor::Cyan.on_default().bold())
+    .placeholder(styling::AnsiColor::Magenta.on_default())
+    .valid(styling::AnsiColor::Green.on_default())
+    .invalid(styling::AnsiColor::Red.on_default());
+
 pub fn cli() -> Command {
     Command::new("qplug")
         .about("Q-Sys plugin Development tool.")
+        .color(ColorChoice::Always)
+        .styles(STYLES)
+        .version(crate_version!())
+        .author(crate_authors!())
         .subcommand_required(true)
         .arg_required_else_help(true)
         // New
@@ -16,7 +32,13 @@ pub fn cli() -> Command {
                 .arg(arg!(<str> "Name of the plugin."))
                 .arg_required_else_help(true)
                 .arg(
-                    Arg::new("Enable Git")
+                    Arg::new("Disabe Git")
+                        .long("no-git")
+                        .default_value("false")
+                        .action(ArgAction::SetTrue),
+                ),
+        )
+        .subcommand(
             Command::new("init")
                 .about("Initialize the current directory as a plugin.")
                 .arg(
@@ -25,7 +47,18 @@ pub fn cli() -> Command {
                         .default_value("false")
                         .action(ArgAction::SetTrue),
                 )
-                .arg(Arg::new("Specify Base Plugin").long("repo")),
+                .arg(
+                    Arg::new("Disable Lua Definitions")
+                        .long("no-defs")
+                        .default_value("false")
+                        .action(ArgAction::SetTrue),
+                )
+                .arg(
+                    Arg::new("Disable Template Creation")
+                        .long("no-template")
+                        .default_value("false")
+                        .action(ArgAction::SetTrue),
+                ),
         )
         // Build
         .subcommand(
