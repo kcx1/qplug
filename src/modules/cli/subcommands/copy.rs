8d4cc676947d002ea21aef0ccfe9e49f1a47fd3c
Casey C <kcx1codes@gmail.com>
2024-09-04 13:38:45 -0400

    Fix: Removes debug print from format! macro to prevent quotes being added

--- src/modules/cli/subcommands/copy.rs
+++ src/modules/cli/subcommands/copy.rs
@@ -2,7 +2,7 @@ use directories;
 
 use crate::files::find_marker_file;
 
-#[cfg(windows)]
+// #[cfg(windows)]
 pub fn copy_to_plugin_directory() -> Result<u64, std::io::Error> {
     let user_dir = directories::UserDirs::new().expect("Unable to locate user dir.");
     let docs = user_dir.document_dir().expect("Unable to locate docs dir.");
@@ -10,10 +10,11 @@ pub fn copy_to_plugin_directory() -> Result<u64, std::io::Error> {
     let marker_file = find_marker_file(None).expect("You might not be in a plugin directory.");
     let file_name = marker_file
         .file_name()
-        .expect("Compiled qplug file not found. Please build or compile it first.");
+        .expect("Compiled qplug file not found. Please build or compile it first.")
+        .to_string_lossy();
 
-    let source_file = marker_file.join(format!("{:?}.qplug", file_name));
-    let destination = plugin_dir.join(format!("{:?}.qplug", file_name));
+    let source_file = marker_file.join(format!("{file_name}.qplug"));
+    let destination = plugin_dir.join(format!("{file_name}.qplug"));
 
     println!(
         "Copying from {} to {}",
