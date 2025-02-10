65b8b918f2c056543a693c46085e5977ebc6bc99
Casey C <kcx1codes@gmail.com>
2025-02-10 16:24:27 -0500

    WIP: Migrating repo to use anyhow.

--- src/modules/config.rs
+++ src/modules/config.rs
@@ -41,7 +41,7 @@ pub struct Config<'a> {
 }
 
 impl Config<'_> {
-    pub fn from_user_config(user_config: &UserConfig) -> Self {
+    pub fn from_user_config(user_config: &UserConfig) -> anyhow::Result<Self> {
         // Internal implementation as a callable
         let default_build_tool = || {
             crate::cli::subcommands::build::default_build_tool();
@@ -65,7 +65,7 @@ impl Config<'_> {
                 if template_str.starts_with("http") {
                     Template::Url(template_str.to_owned())
                 } else {
-                    Template::FileSystem(PathBuf::from_str(&template_str).unwrap())
+                    Template::FileSystem(PathBuf::from_str(&template_str)?)
                 }
             }
             _ => Template::InMemoryDir(&TEMPLATE_DIR),
@@ -85,16 +85,16 @@ impl Config<'_> {
         };
 
         let qsys_plugin_dir = match &user_config.plugin_dir {
-            Value::String(s) => Some(PathBuf::from_str(&s.to_str().unwrap()).unwrap()),
+            Value::String(s) => Some(PathBuf::from_str(&s.to_str().unwrap())?),
             _ => None,
         };
 
-        Config {
+        Ok(Config {
             build_tool,
             template,
             me,
             plugin_dir: qsys_plugin_dir,
-        }
+        })
     }
 }
 
@@ -107,11 +107,11 @@ pub struct UserConfig {
 }
 
 impl UserConfig {
-    pub fn new(lua: &Lua) -> UserConfig {
-        let user_config = match find_config_file() {
+    pub fn new(lua: &Lua) -> anyhow::Result<UserConfig> {
+        let user_config = match find_config_file()? {
             Some(path) => {
                 // Create a function that will return the table form the user config and call it
-                lua.load(fs::read_to_string(&path).unwrap())
+                lua.load(fs::read_to_string(&path)?)
                     .into_function()
                     .unwrap()
                     .call(Nil)
@@ -128,27 +128,27 @@ impl UserConfig {
 
         overload_global_config(&user_config, None, lua);
 
-        UserConfig {
+        Ok(UserConfig {
             external_template: user_config.get("external_template").unwrap_or(Value::Nil),
             build_tool: user_config.get("build_tool").unwrap_or(Value::Nil),
             me: user_config.get("me").unwrap_or(Value::Nil),
             plugin_dir: user_config.get("plugin_dir").unwrap_or(Value::Nil),
-        }
+        })
     }
 }
 
-pub fn find_config_file() -> Option<PathBuf> {
-    fn return_config(config_file: PathBuf) -> Option<PathBuf> {
+pub fn find_config_file() -> anyhow::Result<Option<PathBuf>> {
+    fn return_config(config_file: PathBuf) -> anyhow::Result<Option<PathBuf>> {
         if config_file.exists() {
-            return Some(config_file);
+            return Ok(Some(config_file));
         }
-        None
+        Ok(None)
     }
     // Check in XDG config directories (Linux, macOS)
     let base_dirs = BaseDirs::new().expect("No User Directory found");
     let mut config_file = base_dirs.config_dir().join("qplug/qplug.lua"); // ~/.config on Linux/macOS, AppData/Roaming on Windows
-    match return_config(config_file) {
-        Some(config) => Some(config),
+    match return_config(config_file)? {
+        Some(config) => Ok(Some(config)),
         None => {
             config_file = base_dirs.home_dir().join(".qplug.lua");
             return_config(config_file)
@@ -160,30 +160,26 @@ fn overload_global_config<'a>(
     user_config: &'a Table,
     local_config: Option<PathBuf>,
     lua: &Lua,
-) -> &'a Table {
+) -> anyhow::Result<&'a Table> {
     // Either User provided config or find a marker file
     let overload_config =
         local_config.or_else(|| find_project_dir(Some(&pwd())).map(|path| path.join(MARKER_FILE)));
 
-    if overload_config.is_none() {
-        return user_config;
+    match overload_config {
+        None => Ok(user_config),
+        Some(overload_config) => {
+            let new_config: Table = lua
+                .load(fs::read_to_string(overload_config)?)
+                .into_function()
+                .unwrap()
+                .call(Nil)
+                .unwrap();
+
+            let _ = new_config.for_each(|key: Value, val: Value| user_config.set(key, val));
+
+            Ok(user_config)
+        }
     }
-
-    let new_config: Table = lua
-        .load(fs::read_to_string(overload_config.unwrap()).unwrap())
-        .into_function()
-        .unwrap()
-        .call(Nil)
-        .unwrap();
-
-    new_config
-        .for_each(|key: Value, val: Value| {
-            user_config.set(key, val).unwrap();
-            Ok(())
-        })
-        .unwrap();
-
-    user_config
 }
 
 #[cfg(test)]
@@ -194,9 +190,10 @@ mod tests {
 
     // INFO: This test only works if you don't have a config file in your home directory.
     #[test]
-    fn test_find_config_file_none() {
+    fn test_find_config_file_none() -> anyhow::Result<()> {
         let result = find_config_file();
-        assert!(result.is_none());
+        assert!(result?.is_none());
+        Ok(())
     }
 
     fn test_return_config(config_file: PathBuf) -> Option<PathBuf> {
@@ -224,18 +221,22 @@ mod tests {
 
     // Test the `find_config_file` function with a config file in the XDG config directory.
     #[test]
-    fn test_find_config_file_in_xdg_config() {
-        let temp_dir = tempdir().unwrap();
+    fn test_find_config_file_in_xdg_config() -> anyhow::Result<()> {
+        let temp_dir = tempdir()?;
         let config_dir = temp_dir.path().join("config/qplug");
         let config_file = config_dir.join("qplug.lua");
 
         // Create the directory and file
-        fs::create_dir_all(&config_dir).unwrap();
-        fs::write(&config_file, get_dummy_config()).unwrap();
+        fs::create_dir_all(&config_dir)?;
+        fs::write(&config_file, get_dummy_config())?;
 
         // Mock the BaseDirs::config_dir() to return our temp_dir's config path
-        let result = test_return_config(config_file.clone());
-        assert_eq!(result, Some(config_file.clone()));
+        assert_eq!(
+            test_return_config(config_file.clone()),
+            Some(config_file.clone())
+        );
+
+        Ok(())
 
         // tempdir automatically cleans up when it goes out of scope
     }
@@ -259,11 +260,12 @@ mod tests {
 
     // INFO: This test only works if you don't have a config file in your home directory.
     #[test]
-    fn test_get_config_default() {
+    fn test_get_config_default() -> anyhow::Result<()> {
         let lua = Lua::new();
-        let config = UserConfig::new(&lua);
+        let config = UserConfig::new(&lua)?;
 
         assert_eq!(config.build_tool, Value::Nil);
         assert_eq!(config.external_template, Value::Nil);
+        Ok(())
     }
 }
