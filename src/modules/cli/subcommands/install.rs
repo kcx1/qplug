28429c2bafcb9bff78650c5e4990ca6a3328c787
Casey C <kcx1codes@gmail.com>
2025-05-14 15:42:44 -0400

    WIP: Implements base logic to install encryption and legacy build tools

--- src/modules/cli/subcommands/install.rs
+++ src/modules/cli/subcommands/install.rs
@@ -27,8 +27,8 @@ pub fn install(installable: Option<&Installables>) -> anyhow::Result<()> {
     if let Some(installable) = installable {
         match installable {
             Installables::Definitions => install_definitions()?,
-            Installables::Encryption => println!("Encryption"),
-            Installables::LegacyBuild => println!("LegacyBuild"),
+            Installables::Encryption => install_encryption()?,
+            Installables::LegacyBuild => install_legacy_build()?,
         };
     };
     match installable {
@@ -38,7 +38,7 @@ pub fn install(installable: Option<&Installables>) -> anyhow::Result<()> {
 }
 
 fn install_definitions() -> anyhow::Result<()> {
-    let url = "https://github.com/kcx1/Q-Sys-Defs";
+    let url = "https://github.com/kcx1/Q-SYS-LuaLS-Definitions";
     let defs_path = user_config().join(QPLUG_DIR); // ~/.config on Linux/macOS, AppData/Roaming on Windows
     let defs = Repository::clone(&url, defs_path)
         .context(format!("Failed to clone template repo: {url}"))?
@@ -61,5 +61,17 @@ fn install_encryption() -> anyhow::Result<()> {
         .to_path_buf();
 
     println!("Encryption Tool has been installed to: {:?}", tool);
+    Ok(())
+}
+
+fn install_legacy_build() -> anyhow::Result<()> {
+    let url = "https://bitbucket.org/qsc-communities/basicpluginframework/";
+    let tool_path = user_config().join(QPLUG_DIR);
+    let tool = Repository::clone(url, tool_path)
+        .context(format!("Failed to clone template repo: {url}"))?
+        .path()
+        .to_path_buf();
+
+    println!("LegacyBuild Tool has been installed to: {:?}", tool);
     Ok(())
 }
