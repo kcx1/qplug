c3186ef7b14d5192a894001773bcf732aac8df00
Casey C <kcx1codes@gmail.com>
2025-05-14 14:48:00 -0400

    Feat: Adds the luarc.json handler
    This is to prepare for moving away from local definitions completely

--- src/modules/cli/subcommands/install.rs
+++ src/modules/cli/subcommands/install.rs
@@ -1,7 +1,8 @@
 use anyhow::Context;
 use clap::ValueEnum;
-use directories::BaseDirs;
 use git2::Repository;
+
+use crate::{files::user_config, globals::QPLUG_DIR, modules::lua_lsp::write_lsp_file};
 
 #[derive(ValueEnum, Clone, Debug)]
 #[clap(rename_all = "lower")]
@@ -38,15 +39,15 @@ pub fn install(installable: Option<&Installables>) -> anyhow::Result<()> {
 
 fn install_definitions() -> anyhow::Result<()> {
     let url = "https://github.com/kcx1/Q-Sys-Defs";
-    let base_dirs =
-        BaseDirs::new().expect("Oops! No valid home diretory could be found on this system");
-    let defs_path = base_dirs.config_dir().join("qplug/"); // ~/.config on Linux/macOS, AppData/Roaming on Windows
+    let defs_path = user_config().join(QPLUG_DIR); // ~/.config on Linux/macOS, AppData/Roaming on Windows
     let defs = Repository::clone(&url, defs_path)
         .context(format!("Failed to clone template repo: {url}"))?
         .path()
         .to_path_buf();
 
     println!("Definitions have been installed to: {:?}", defs);
+
+    write_lsp_file()?;
 
     Ok(())
 }
--- src/modules/lua_lsp.rs
+++ src/modules/lua_lsp.rs
@@ -1,16 +1,128 @@
-use std::fs;
+use std::{fs, path::PathBuf};
 
-use serde_json::Value;
+use serde_json::{json, Value};
 
-use crate::globals::QPLUG_CONFIG;
+use crate::globals::QPLUG_DIR;
 
-use super::config::find_config_file;
+use super::{config::find_config_file, files::user_config};
 
-pub fn update_lsp_config() -> anyhow::Result<()> {
-    if let Some(user_config) = find_config_file(QPLUG_CONFIG) {
-        let buffered_luarc = fs::read_to_string(user_config)?;
-        let luarc: Value = serde_json::from_str(&buffered_luarc)?;
+pub fn get_lua_lsp_file() -> Option<PathBuf> {
+    find_config_file("luarc.json")
+}
+
+pub fn write_lsp_file() -> anyhow::Result<()> {
+    let mut luarc: Value;
+    // If there is a user config somewhere
+    match get_lua_lsp_file() {
+        Some(config_file) => {
+            let buffered_luarc = fs::read_to_string(&config_file)?;
+            luarc = serde_json::from_str(&buffered_luarc)?;
+        }
+        None => luarc = serde_json::from_str(r#"{}"#)?,
     }
+    luarc["workspac"]["library"] = append_definitons(&luarc, user_config().join("definitions"));
+
+    let modified_doc = serde_json::to_string_pretty(&luarc)?;
+    fs::write(
+        user_config().join(QPLUG_DIR).join("luarc.json"),
+        modified_doc,
+    )?;
 
     Ok(())
+}
+
+fn wrkspc_lib_array(json_val: &Value) -> Vec<&str> {
+    if json_val["wokspace"].is_null() || json_val["wokspace"]["library"].is_null() {
+        return vec![];
+    }
+    json_val["wokspace"]["library"]
+        .as_array()
+        .unwrap()
+        .iter()
+        .map(|s| s.as_str().unwrap())
+        .collect::<Vec<&str>>()
+}
+
+#[allow(dead_code)]
+fn has_definitions(json_val: &Value) -> anyhow::Result<bool> {
+    if json_val["wokspace"].is_null() {
+        return Ok(false);
+    }
+    if json_val["wokspace"]["library"].is_null() {
+        return Ok(false);
+    }
+    Ok(wrkspc_lib_array(&json_val).contains(&"/some/path"))
+}
+
+/// Append a definition file to the vec.
+/// Be sure to update the json object!
+fn append_definitons(json_val: &Value, def_dir: PathBuf) -> Value {
+    let mut result = wrkspc_lib_array(&json_val);
+    result.push(
+        def_dir
+            .to_str()
+            .expect("Path is not proper unicode and cannot be converted."),
+    );
+    json!(result)
+}
+
+#[cfg(test)]
+mod tests {
+
+    use std::path::PathBuf;
+
+    use serde_json::Value;
+
+    use super::{append_definitons, has_definitions};
+    static EXISTING_DEF: &'static str = r#"{"wokspace" : {"library": ["/some/path"]}}"#;
+    static DIFFERENT_DEFS: &'static str = r#"{"wokspace" : {"library": ["/some/different/path"]}}"#;
+    static NO_LIBS: &'static str = r#"{"wokspace" : "Not library"}"#;
+    static NO_WRKSPACE: &'static str = r#"{}"#;
+    static MULTI_DEFS: &'static str = r#"["/some/different/path", "/some/path"]"#;
+
+    #[test]
+    fn test_serde_json_parsing() {
+        let parsed: Value = serde_json::from_str(EXISTING_DEF).unwrap();
+        let results = parsed["wokspace"]["library"]
+            .as_array()
+            .unwrap()
+            .iter()
+            .map(|s| s.as_str().unwrap())
+            .collect::<Vec<&str>>();
+        assert_eq!(results, vec!["/some/path"]);
+        assert!(true)
+    }
+
+    #[test]
+    fn test_has_defs() {
+        let parsed: Value = serde_json::from_str(EXISTING_DEF).unwrap();
+        assert!(has_definitions(&parsed).unwrap());
+    }
+
+    #[test]
+    fn test_has_no_defs() {
+        let diff_defs: Value = serde_json::from_str(DIFFERENT_DEFS).unwrap();
+        let no_libs: Value = serde_json::from_str(NO_LIBS).unwrap();
+        let no_wrkspc: Value = serde_json::from_str(NO_WRKSPACE).unwrap();
+        assert!(!has_definitions(&diff_defs).unwrap());
+        assert!(!has_definitions(&no_libs).unwrap());
+        assert!(!has_definitions(&no_wrkspc).unwrap());
+    }
+
+    #[test]
+    fn test_append() {
+        let different_defs: Value = serde_json::from_str(DIFFERENT_DEFS).unwrap();
+        let different_paths_res = append_definitons(&different_defs, PathBuf::from("/some/path"));
+        let different_paths_expected: Value = serde_json::from_str(MULTI_DEFS).unwrap();
+
+        let no_wrkspc: Value = serde_json::from_str(NO_WRKSPACE).unwrap();
+        let no_wrkspc_res: Value = append_definitons(&no_wrkspc, PathBuf::from("/some/path"));
+
+        println!("{} {}", different_paths_res, different_paths_expected);
+        assert_eq!(different_paths_res, different_paths_expected);
+        assert_eq!(
+            no_wrkspc_res,
+            serde_json::from_str::<Value>(r#"["/some/path"]"#).unwrap()
+        );
+    }
 }
